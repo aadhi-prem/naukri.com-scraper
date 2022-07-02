2f834ff5f5a6694876146c9ec384b94cdd665a8d
aadhi-prem <aditya.premjit@gmail.com>
2022-07-03 00:47:22 +0530

    Updated with file option

--- nscraper.py
+++ nscraper.py
@@ -2,6 +2,7 @@
 
 import requests
 import pprint
+import bs4
 
 
 url= "https://www.naukri.com/jobapi/v3/search?noOfResults=20&urlType=search_by_key_loc&searchType=adv&keyword=pyhton&location=bangalore%2Fbengaluru&pageNo=1&k=pyhton&l=bangalore%2Fbengaluru&seoKey=pyhton-jobs-in-bangalore-bengaluru&src=jobsearchDesk&latLong="
@@ -9,6 +10,14 @@ url= "https://www.naukri.com/jobapi/v3/search?noOfResults=20&urlType=search_by_k
 headers={"appid" : "109","systemid" : "109"}
 r = requests.get(url,headers=headers)
 data = r.json()
-
-pprint.pprint(data)
+jobid = 1
+for i in data['jobDetails']:
+	#print(,"\n",i['companyName'])
+	soup=bs4.BeautifulSoup(i['jobDescription'],'html.parser')
+	#print(,"\n\n")
+	with open (str(jobid)+'.txt','w') as f:
+		f.write(i['title'])
+		f.write(i['companyName'])
+		f.write(soup.get_text())
+	jobid+=1
 
